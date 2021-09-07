a535496c4892af86e73978dc8f6afd98d812d6a3
vigpandian <vignesh.pandian@gmail.com>
2021-09-07 11:03:43 +0000

    Launchpad added, New Fiori wizard app

---
+++ app/office_supplies/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("hc450.officesupplies.officesupplies.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
